46a422ca957ba43363b3c8447460a5ee71bee623
ArkenStorm <shadowarchon7@gmail.com>
2021-08-12 19:10:57 -0600

    Initial basic files committed; no actual code yet.

---
+++ dragon.js
@@ -0,0 +1,1 @@
+// There is absolutely nothing here yet, as I'm trying to create a framework to go off of first. This is kind of selfish of me, but I'm using it as an initial test for upgraded ze kaiser.
