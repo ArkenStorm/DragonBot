6db0d26d94f7d98c9c294e16e3e8dc8d1af2ce0a
ArkenStorm <shadowarchon7@gmail.com>
2021-08-12 19:15:31 -0600

    reformat of comment

--- dragon.js
+++ dragon.js
@@ -1,1 +1,2 @@
-// There is absolutely nothing here yet, as I'm trying to create a framework to go off of first. This is kind of selfish of me, but I'm using it as an initial test for upgraded ze kaiser.
+// There is absolutely nothing here yet, as I'm trying to create a framework to go off of first.
+// This is kind of selfish of me, but I'm using it as an initial test for upgraded ze kaiser.
